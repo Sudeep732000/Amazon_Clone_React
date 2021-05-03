7e34a5ce712d3935a86f49de99703ca8207dc2b3
Sudeep Balagopal <60956438+Sudeep732000@users.noreply.github.com>
2021-05-03 19:23:29 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 // eslint-disable-next-line no-unused-vars
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBdTQm1ffKg47NISMFks-x7Lghrp541b8g",
-    authDomain: "clone-9746c.firebaseapp.com",
-    projectId: "clone-9746c",
-    storageBucket: "clone-9746c.appspot.com",
-    messagingSenderId: "56604934938",
-    appId: "1:56604934938:web:5f1787383a0d41d483e2a3",
-    measurementId: "G-NRYDC7VBNN"
+    apiKey: "Your Firebase Key",
+    authDomain: "Your Domain",
+    projectId: "Your ID",
+    storageBucket: "YOur config",
+    messagingSenderId: "Given ID",
+    appId: "Your ID",
+    measurementId: "ID"
   });
   const db = firebase.firestore();
   const auth = firebase.auth();
